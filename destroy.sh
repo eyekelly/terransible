020c48bcbecb9a9b90f0b8fff3f85b0f1fe046b3
Ian Kelly <i.kelly@ubiquedigital.com>
2018-03-07 12:14:57 +0000

    adding bash scripts for starting and destroying the webserver

---
+++ destroy.sh
@@ -0,0 +1,1 @@
+(cd terraform/modules/instance/ ; terraform destroy)
---
+++ start.sh
@@ -0,0 +1,20 @@
+# START THE SERVER AND RUN THE ANSIBLE PLAYBOOK
+# This script assumes you have
+# terraform and ansible installed
+# your PATH variable set up with both applications
+# your keys to access AWS set up
+# you've edited the terraform file to apply your own key
+# you've got a security group called basic-access-sg in your VPC
+# you're running this script from directly in the "Terransible" directory
+
+#(cd terraform/modules/instance/ ; terraform init )
+#(cd terraform/modules/instance/ ; terraform apply --auto-approve)
+#(cd terraform/modules/instance/ ; host='terraform output public_ip') 
+host=$(cd terraform/modules/instance/ ; terraform output public_ip)
+cat <<EOF > ansible/wordpress_ansible/hosts
+[allservers]
+$host
+[wordpress]
+$host
+EOF
+(cd ansible/wordpress_ansible ; ansible-playbook playbook.yml -i hosts --private-key ~/.ssh/ud_devops_EU.pem)
